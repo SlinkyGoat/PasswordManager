11bd0caadb9db91a2f2c976820c6885c041f1118
Adam-Ustby <adamustby@gmail.com>
2023-07-17 16:37:03 -0500

    started structure file
    To simplify this project I wanted to read from the file to create a data structure to handle all the adjustments and then rewrite file if any changes are made

---
+++ src/DataStructure.java
@@ -0,0 +1,44 @@
+import java.util.HashMap;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+
+public class DataStructure {
+
+	public HashMap<String, String> collectData(){
+		
+		HashMap<String, String> data = new HashMap<String, String>();
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(System.getenv("APPDATA") + "\\PM\\" + "test.txt"));
+		} catch (FileNotFoundException e) {
+			System.out.println("File Reader unable to be created in collectData()");
+			return null;
+		}
+		
+		String keyBuffer = "";
+		String valueBuffer = "";
+		while(true) {
+			try {
+				keyBuffer = reader.readLine();
+				valueBuffer = reader.readLine();
+			} catch (IOException e) {
+				System.out.println("Error reading line in collectData() loop");
+			}
+			if(keyBuffer == null) {
+				break;
+			}
+			data.put(keyBuffer, valueBuffer);
+		}
+		try {
+			reader.close();
+		} catch (IOException e) {
+			System.out.println("Reader failed to close in collectData()");
+		}
+		
+		return data;
+		
+	}
+	
+}
