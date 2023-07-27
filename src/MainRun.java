f2e31572a5483377c3fe0989f7d22c5e3bfad591
Adam-Ustby <adamustby@gmail.com>
2023-07-27 17:28:36 -0500

    window with title image is done
    frame.setVisible(true) needs to be at the bottom to ensure the different components (such as the JLabel) are also visible. If this line is above the initialization/adding of the JLabel it will NOT appear in the window

--- src/MainRun.java
+++ src/MainRun.java
@@ -1,9 +1,30 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
 
 public class MainRun {
 
+	private static void createAndShowGUI() {
+		final JFrame frame = new JFrame("Password Manager");
+		
+		//initialize window
+		frame.setSize(1000,800);
+		frame.setLayout(null);
+		frame.getContentPane().setBackground(Color.WHITE);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//create Title image
+		JLabel title = new JLabel(new ImageIcon("assets\\PM_Title.png"));
+		title.setBounds(0,0,1000,150);
+		frame.add(title);
+		
+		//finishing frame
+		frame.setVisible(true);
+		
+	}
+	
 	public static void main(String[] args) {
 		DataStorage storage = new DataStorage();
 		storage.createPasswordManagerDirectory();
@@ -11,6 +32,14 @@ public class MainRun {
 		
 		DataStructure structure = new DataStructure();
 		structure.collectData();
+		
+//		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+//			public void run() {
+//				createAndShowGUI();
+//			}
+//		});
+		
+		createAndShowGUI();
 		
 		
 	}
