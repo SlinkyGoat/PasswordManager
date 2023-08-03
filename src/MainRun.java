98228a850b5760182fd8854a087af8ed5a089e2a
Adam-Ustby <adamustby@gmail.com>
2023-08-03 16:09:18 -0500

    starting transfer to GreidBagLayout format

--- src/MainRun.java
+++ src/MainRun.java
@@ -5,6 +5,14 @@ import java.io.File;
 import java.io.IOException;
 
 public class MainRun {
+	
+	final static boolean shouldFill = true;
+	final static boolean shouldWeightX = true;
+	final static boolean RIGHT_TO_LEFT = false;
+	
+	public static void addComponentsToPane(Container pane) {
+		
+	}
 
 	private static void createAndShowGUI() {
 		final JFrame frame = new JFrame("Password Manager");
@@ -17,22 +25,35 @@ public class MainRun {
 		frame.setResizable(false);
 		
 		//create Title image
-		JLabel title = new JLabel(new ImageIcon("assets\\PM_Title.png"));
-		title.setBounds(0,0,1000,150);
-		frame.add(title);
+//		JLabel title = new JLabel(new ImageIcon("assets\\PM_Title.png"));
+//		title.setBounds(0,0,1000,150);
+//		frame.add(title);
+//		
+//		//create search bar at top of screen
+//		//creating the text field
+//		JTextField searchText = new JTextField();
+//		searchText.setBounds(65, 200, 150, 30);
+//		frame.add(searchText);
+//		
+//		//creating the search button
+//		JButton searchButton = new JButton("Search");
+//		searchButton.setBounds(235, 200, 100, 30);
+//		frame.add(searchButton);
+//		
+//		//creating add item button
+//		JButton addItemButton = new JButton("Add Item");
+//		addItemButton.setBounds(65, 720, 100, 30);
+//		frame.add(addItemButton);
 		
-		//create search bar at top of screen
-		//creating the text field
-		JTextField searchText = new JTextField();
-		searchText.setBounds(65, 200, 150, 30);
-		frame.add(searchText);
+		//creating table
 		
-		//creating the search button
-		JButton searchButton = new JButton("Search");
-		searchButton.setBounds(235, 200, 100, 30);
-		frame.add(searchButton);
+		// start y=280, end y=670
+		// add scrollpane
+		
+		addComponentsToPane(frame.getContentPane());
 		
 		//finishing frame
+		frame.pack();
 		frame.setVisible(true);
 		
 	}
@@ -45,13 +66,13 @@ public class MainRun {
 		DataStructure structure = new DataStructure();
 		structure.collectData();
 		
-//		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-//			public void run() {
-//				createAndShowGUI();
-//			}
-//		});
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
 		
-		createAndShowGUI();
+//		createAndShowGUI();
 		
 		
 	}
