7e9c6d9dad5d8de2f9bed5a171528099c5e5c492
Adam-Ustby <adamustby@gmail.com>
2023-08-03 17:55:22 -0500

    gui started with gridbaglayout manager

--- src/MainRun.java
+++ src/MainRun.java
@@ -10,36 +10,60 @@ public class MainRun {
 	final static boolean shouldWeightX = true;
 	final static boolean RIGHT_TO_LEFT = false;
 	
-	public static void addComponentsToPane(Container pane) {
+	public static void addComponentsToPane(Container pane, String[][] data) {
+		if (RIGHT_TO_LEFT) {
+            pane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+        }
+		pane.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		if(shouldFill) {
+			c.fill = GridBagConstraints.HORIZONTAL;
+		}
+		
+		// title image at top of screen
+		JLabel title = new JLabel(new ImageIcon("assets\\PM_Title.png"));
+		c.gridx = 0;
+		c.gridy = 0;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		pane.add(title, c);
+		
+		JTextField searchBar = new JTextField();
+		c.gridx = 0;
+		c.gridy = 1;
+		c.insets = new Insets(0,65,0,65);
+//		c.fill = GridBagConstraints.NONE;
+		pane.add(searchBar, c);
+		
+		JButton searchButton = new JButton("Search");
+		c.gridx = 1;
+		c.gridy = 1;
+		c.insets = new Insets(0,0,0,65);
+		pane.add(searchButton, c);
+		
+		String column[]={"Service","Password"};
+		JTable table = new JTable(data, column);
+		c.gridx = 0;
+		c.gridy = 2;
+		c.insets = new Insets(30,65,30,0);
+		c.anchor = GridBagConstraints.LINE_START;
+		c.weighty = 1.0;
+		pane.add(table, c);
+		
+		JScrollPane scroll = new JScrollPane(table);
+		pane.add(scroll, c);
 		
 	}
 
-	private static void createAndShowGUI() {
+	private static void createAndShowGUI(String[][] data) {
 		final JFrame frame = new JFrame("Password Manager");
 		
 		//initialize window
-		frame.setSize(1000,800);
-		frame.setLayout(null);
+//		frame.setSize(1000,800);
+//		frame.setLayout(null);
 		frame.getContentPane().setBackground(Color.WHITE);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(false);
-		
-		//create Title image
-//		JLabel title = new JLabel(new ImageIcon("assets\\PM_Title.png"));
-//		title.setBounds(0,0,1000,150);
-//		frame.add(title);
-//		
-//		//create search bar at top of screen
-//		//creating the text field
-//		JTextField searchText = new JTextField();
-//		searchText.setBounds(65, 200, 150, 30);
-//		frame.add(searchText);
-//		
-//		//creating the search button
-//		JButton searchButton = new JButton("Search");
-//		searchButton.setBounds(235, 200, 100, 30);
-//		frame.add(searchButton);
-//		
+						
 //		//creating add item button
 //		JButton addItemButton = new JButton("Add Item");
 //		addItemButton.setBounds(65, 720, 100, 30);
@@ -50,10 +74,11 @@ public class MainRun {
 		// start y=280, end y=670
 		// add scrollpane
 		
-		addComponentsToPane(frame.getContentPane());
+		addComponentsToPane(frame.getContentPane(), data);
 		
 		//finishing frame
 		frame.pack();
+		frame.setMinimumSize(frame.getMinimumSize());
 		frame.setVisible(true);
 		
 	}
@@ -65,10 +90,11 @@ public class MainRun {
 		
 		DataStructure structure = new DataStructure();
 		structure.collectData();
+		String data[][] = structure.hashToArr();
 		
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				createAndShowGUI();
+				createAndShowGUI(data);
 			}
 		});
 		
