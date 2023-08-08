88503c2a8ead6bf0ca1a22da3e3d72ac812fed7a
Adam-Ustby <adamustby@gmail.com>
2023-08-08 18:55:08 -0500

    gui generally done, starting to work on functionality now

--- src/MainRun.java
+++ src/MainRun.java
@@ -25,18 +25,22 @@ public class MainRun {
 		c.gridx = 0;
 		c.gridy = 0;
 		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridwidth = 3;
 		pane.add(title, c);
 		
 		JTextField searchBar = new JTextField();
 		c.gridx = 0;
 		c.gridy = 1;
+		c.gridwidth = 3;
+		// Insets(top, left, bottom, right)
 		c.insets = new Insets(0,65,0,65);
 //		c.fill = GridBagConstraints.NONE;
 		pane.add(searchBar, c);
 		
 		JButton searchButton = new JButton("Search");
-		c.gridx = 1;
+		c.gridx = 3;
 		c.gridy = 1;
+		c.gridwidth = 1;
 		c.insets = new Insets(0,0,0,65);
 		pane.add(searchButton, c);
 		
@@ -44,13 +48,36 @@ public class MainRun {
 		JTable table = new JTable(data, column);
 		c.gridx = 0;
 		c.gridy = 2;
-		c.insets = new Insets(30,65,30,0);
+		c.gridwidth = 3;
+		c.insets = new Insets(30,65,30,30);
 		c.anchor = GridBagConstraints.LINE_START;
 		c.weighty = 1.0;
 		pane.add(table, c);
 		
 		JScrollPane scroll = new JScrollPane(table);
 		pane.add(scroll, c);
+		
+		JButton addButton = new JButton("Add");
+		c.gridwidth = 1;
+		c.gridx = 0;
+		c.gridy = 3;
+		pane.add(addButton, c);
+		
+		//c.insets = new Insets(30,65,30,30);
+		
+		JTextField newServiceName = new JTextField();
+		c.gridx = 1;
+		c.gridwidth = 1;
+		c.ipadx = 400;
+		c.insets = new Insets(30, 10, 30, 10);
+		pane.add(newServiceName, c);
+		
+		JTextField newPasswordName = new JTextField();
+		c.gridx = 2;
+		c.gridwidth = 1;
+		c.ipadx = 0;
+		c.insets = new Insets(30, 10, 30, 30);
+		pane.add(newPasswordName, c);
 		
 	}
 
